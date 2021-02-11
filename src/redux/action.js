7a5e7cecb36c48dfea397263b6759ed1effad1fa
”sovsianykov” <swebproger@gmail.com>
2021-02-11 22:11:21 +0200

    good

--- src/redux/action.js
+++ src/redux/action.js
@@ -6,12 +6,10 @@ export function fetchWeather( ) {
     return  async dispatch =>
     {
 
-        const response = await fetch('api.openweathermap.org/data/2.5/weather?lat=50.37961&lon=30.36742&appid=d409c3b0705292f3d52ab9675afd8d8e&units=metric')
+        const response = await fetch('https://api.openweathermap.org/data/2.5/weather?lat=50.37961&lon=30.36742&appid=d409c3b0705292f3d52ab9675afd8d8e&units=metric')
             .then(response => response.json())
-            .then(response => console.table(response))
-
         dispatch({type: GET_HOME_WEATHER, payload : response})
-        console.log('i am response' + response)
+        console.log(response.main.temp)
 
 
 
